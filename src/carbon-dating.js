a23485da0790f2a1360e54c5ed10f7365b8738ec
Denis <crippton@gmail.com>
2020-10-04 06:11:02 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  if ((typeof (sampleActivity) !== 'string') || (typeof (sampleActivity) === 'undefined')) {
+    // console.log(typeof (sampleActivity));
+    //  console.log('false');
+    return false;
+  } else if (!isNaN(sampleActivity) && (sampleActivity > 0)) {
+    let k = Math.LN2 / HALF_LIFE_PERIOD;
+    let t = Math.round(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+    if ((t === Infinity) || (t === undefined) || (t < 0) ) {
+        // console.log('false');
+      return false;
+    } else {
+      //console.log(sampleActivity);
+      //  console.log(t);
+      return t;
+    }
+
+  }
+  //console.log('false');
+  return false;
+}
--- src/dev.js
+++ src/dev.js
@@ -1,25 +1,53 @@
-function countCats(matrix) {
-    let result = 0;
-    let temp = [];
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-    matrix.forEach(element => {
-        temp = temp.concat(element);
-        });
-
-    temp.forEach(element => {
-        if (element === '^^') {
-            result += 1;
+function dateSample(sampleActivity) {
+    if ((typeof (sampleActivity) !== 'string') || (typeof (sampleActivity) === 'undefined')) {
+        // console.log(typeof (sampleActivity));
+         console.log('false');
+        return false;
+      } else if (!isNaN(sampleActivity) && (sampleActivity > 0)) {
+        let k = Math.LN2 / HALF_LIFE_PERIOD;
+        let t = Math.round(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+        if ((t === Infinity) || (t === undefined) || (t < 0) ) {
+            console.log('false');
+          return false;
+        } else {
+          //console.log(sampleActivity);
+           console.log(t);
+          return t;
+        }
+    
+      }
+      console.log('false');
+      return false;
     }
-});
-    console.log(matrix);
-    console.log(temp);
 
-    console.log(result);
-    return result;
-};
+// dateSample(3);
+// dateSample(3.312312);
+// dateSample(false);
+// dateSample(null);
+// dateSample(undefined);
+// dateSample([3]);
+// dateSample(['3']);
+// dateSample({
+//     '3.14': '3dec'
+// });
+// dateSample();
+// dateSample('ACTIVITY OVER 9000');
+// dateSample('one');
+// dateSample('');
+// dateSample(' ');
+// dateSample(' \n\t\r');
+// dateSample('1');
+// dateSample('3');
+// dateSample('1');
+// dateSample('9');
+// dateSample('11');
 
-countCats([
-    ['##1', 'dd', '00'],
-    ['^^', '..', 'ss'],
-    ['AA', 'dd', 'Oo']
-]);
+
+dateSample('9000');
+dateSample('15.1');
+dateSample('0');
+dateSample('-5');
+dateSample('-55.8');
